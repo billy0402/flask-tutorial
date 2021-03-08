6e33a14ec855d52cb30a497125d481947b0215a3
Billy Huang <10646003@ntub.edu.tw>
2021-03-09 01:35:10 +0800

    CH3 - 模板

--- app.py
+++ app.py
@@ -1,13 +1,29 @@
-from flask import Flask
+from datetime import datetime
+
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 
 @app.route('/')
 def index():
-    return '<h1>Hello, world!</h1>'
+    return render_template('index.html', current_time=datetime.utcnow())
 
 
 @app.route('/user/<name>')
 def user(name):
-    return f'<h1>Hello, {name}!</h1>'
+    return render_template('user.html', name=name)
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(error):
+    return render_template('500.html'), 500
