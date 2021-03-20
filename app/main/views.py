f320329afcfe1680d8e4f9d31d67d770fa528104
Billy Huang <10646003@ntub.edu.tw>
2021-03-20 18:06:31 +0800

    CH13: 使用者評論

--- app/main/views.py
+++ app/main/views.py
@@ -11,9 +11,10 @@ from flask import (
 from flask_login import current_user, login_required
 
 from . import main
-from .forms import PostForm
+from .forms import PostForm, CommentForm
 from .. import db
-from ..models import Permission, Post
+from ..decorators import permission_required
+from ..models import Permission, Post, Comment
 
 
 @main.route('/', methods=['GET', 'POST'])
@@ -77,10 +78,38 @@ def show_followed():
     return response
 
 
-@main.route('/post/<int:id>')
+@main.route('/post/<int:id>', methods=['GET', 'POST'])
 def show(id):
     post = Post.query.get_or_404(id)
-    return render_template('show.html', posts=[post])
+    form = CommentForm()
+    if form.validate_on_submit():
+        comment = Comment(
+            body=form.body.data,
+            post=post,
+            author=current_user._get_current_object(),
+        )
+        db.session.add(comment)
+        db.session.commit()
+        flash('Your comment has been published.')
+        return redirect(url_for('.show', id=post.id, page=-1))
+
+    page = request.args.get('page', 1, type=int)
+    if page == -1:
+        page = (post.comments.count() - 1) // \
+               current_app.config['FLASKY_COMMENTS_PER_PAGE'] + 1
+    pagination = post.comments.order_by(Comment.timestamp.asc()).paginate(
+        page,
+        per_page=current_app.config['FLASKY_COMMENTS_PER_PAGE'],
+        error_out=False,
+    )
+    comments = pagination.items
+    return render_template(
+        'show.html',
+        posts=[post],
+        form=form,
+        comments=comments,
+        pagination=pagination,
+    )
 
 
 @main.route('/edit/<int:id>', methods=['GET', 'POST'])
@@ -100,3 +129,46 @@ def edit(id):
         return redirect(url_for('.show', id=post.id))
     form.body.data = post.body
     return render_template('edit.html', form=form)
+
+
+@main.route('/moderate')
+@login_required
+@permission_required(Permission.MODERATE)
+def moderate():
+    page = request.args.get('page', 1, type=int)
+    pagination = Comment.query.order_by(Comment.timestamp.desc()).paginate(
+        page,
+        per_page=current_app.config['FLASKY_COMMENTS_PER_PAGE'],
+        error_out=False,
+    )
+    comments = pagination.items
+    return render_template(
+        'moderate.html',
+        comments=comments,
+        pagination=pagination,
+        page=page,
+    )
+
+
+@main.route('/moderate/enable/<int:id>')
+@login_required
+@permission_required(Permission.MODERATE)
+def moderate_enable(id):
+    comment = Comment.query.get_or_404(id)
+    comment.disabled = False
+    db.session.add(comment)
+    db.session.commit()
+    page = request.args.get('page', 1, type=int)
+    return redirect(url_for('.moderate', page=page))
+
+
+@main.route('/moderate/disable/<int:id>')
+@login_required
+@permission_required(Permission.MODERATE)
+def moderate_disable(id):
+    comment = Comment.query.get_or_404(id)
+    comment.disabled = True
+    db.session.add(comment)
+    db.session.commit()
+    page = request.args.get('page', 1, type=int)
+    return redirect(url_for('.moderate', page=page))
--- app/models.py
+++ app/models.py
@@ -130,6 +130,7 @@ class User(UserMixin, db.Model):
         lazy='dynamic',
         cascade='all, delete-orphan',
     )
+    comments = db.relationship('Comment', backref='author', lazy='dynamic')
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -303,6 +304,7 @@ class Post(db.Model):
     body_html = db.Column(db.Text)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    comments = db.relationship('Comment', backref='post', lazy='dynamic')
 
     @staticmethod
     def on_change_body(target, value, old_value, initiator):
@@ -319,3 +321,28 @@ class Post(db.Model):
 
 
 db.event.listen(Post.body, 'set', Post.on_change_body)
+
+
+class Comment(db.Model):
+    __tablename__ = 'comments'
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.Text)
+    body_html = db.Column(db.Text)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    disabled = db.Column(db.Boolean)
+    author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'))
+
+    @staticmethod
+    def on_change_body(target, value, old_value, initiator):
+        allowed_tags = [
+            'a', 'abbr', 'acronym', 'b', 'code', 'em', 'i', 'strong',
+        ]  # 移除段落標籤，僅含字元標籤
+        target.body_html = bleach.linkify(bleach.clean(
+            markdown(value, output_format='html'),
+            tags=allowed_tags,
+            strip=True,
+        ))
+
+
+db.event.listen(Comment.body, 'set', Comment.on_change_body)
